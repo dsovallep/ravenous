0aefec1f572389da00522e0f83c08c9cf2093239
David Ovalle <ds@athento.com>
2024-02-11 15:42:33 -0500

    Eliminados del area de  preparacion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Business from './components/Business/Business.js';
+import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchBar />
+    <BusinessList />
   </React.StrictMode>
 );
 
