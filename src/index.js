9407236b7838cf19b8fbba92428fe4d728bb2bdc
David Ovalle <ds@athento.com>
2024-02-17 10:29:44 -0500

    Adjust the app "App" for render

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import reportWebVitals from './reportWebVitals';
 import Business from './components/Business/Business.js';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
-
+import App from './components/App/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SearchBar />
-    <BusinessList />
+	<App />
   </React.StrictMode>
 );
 
